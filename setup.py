3229a02672a04ea6532a32e6311f41d130bd454b
sawyersteven <sawyerstevenk@gmail.com>
2018-07-22 22:19:22 -0400

    Add setup for pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from m2r import convert
+
+with open("README.md", "r") as f:
+    long_description = convert(f.read())
+
+setuptools.setup(
+    name="SelfishClass",
+    version="0.1",
+    author="SawyerSteven",
+    author_email="sawyerstevenk@gmail.com",
+    description="A minimal alternative to dataclasses and python-attrs",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/sawyersteven/SelfishClass",
+    packages=setuptools.find_packages(),
+    classifiers=(
+		"Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
